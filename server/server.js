0ee08d50e32ec5d1a50637a744a70d164c9522f3
etuong <etuong@gmail.com>
2022-08-10 21:33:59 -0700

    Changed server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,10 @@ server.listen(port, () => {
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://localhost:8080", "https://etuong.github.io"],
+    origin: [
+      "http://localhost:8080",
+      "https://lighthearted-piroshki-24ee03.netlify.app",
+    ],
     credentials: true,
   },
   transports: ["polling", "websocket"],
