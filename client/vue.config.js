ce4bd570552367a2f15cff67e735f683b58d4722
etuong <etuong@gmail.com>
2022-08-10 21:52:24 -0700

    Change public path

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,15 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/who-am-i/'
-    : '/',
+  publicPath: "/",
   transpileDependencies: true,
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === "development") {
       config.devtool = "eval-source-map";
       config.output.devtoolModuleFilenameTemplate = (info) =>
         info.resourcePath.match(/\.vue$/) &&
-          !info.identifier.match(/type=script/)
+        !info.identifier.match(/type=script/)
           ? `webpack-generated:///${info.resourcePath}?${info.hash}`
           : `webpack-yourCode:///${info.resourcePath}`;
 
